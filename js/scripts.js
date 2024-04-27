ed33526f48693b9867112c304061e141169f78a4
Michael Wilson <michaelwilson777@gmail.com>
2024-04-27 12:01:55 -0400

    new commit for class

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,54 @@ const carouselButton = document.getElementById('carouselButton');
 const faIcon = document.getElementById('faButton');
 
 carouselButton.addEventListener('click', function () {
+
     if (faIcon.classList.contains('fa-pause')) {
+
         faIcon.classList.remove('fa-pause');
         faIcon.classList.add('fa-play');
         carousel.pause();
+
     } else {
         faIcon.classList.remove('fa-play');
         faIcon.classList.add('fa-pause');
         carousel.cycle();
     }
-})   
+}) 
+
+async function fetchWeather() {
+   
+   try{
+
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = "North Branford";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+    displayWeather(data)
+
+   } catch (error) {
+    
+    console.error(`There was an error fetching weather data`, error);
+
+   }
+}
+fetchWeather()
+
+
+function displayWeather(data) {
+
+    const temp = data.main.temp;
+    document.querySelector('#weather-temp').textContent = `${Math.round(temp)}\u00B0`;
+
+    const weatherDescription = document.querySelector('#weather-description');
+    weatherDescription.textContent = data.weather[0].description; 
+
+    const image = document.createElement('img');
+    image.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+    const currentWeather = document.querySelector('#weather-icon');
+    
+    currentWeather.appendChild(image);
+}
